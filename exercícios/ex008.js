fe2548293c920ba3b5c5450cde6d55eb447d0b40
Williamcodess <122636223+Williamcodess@users.noreply.github.com>
2023-02-22 14:42:35 -0300

    Criação do exx08

---
+++ exercícios/ex008.js
@@ -0,0 +1,8 @@
+var vel = 50.5
+console.log(`A velocidade do carro é ${vel}Km`)
+if (vel > 60){//condição simples
+    console.log('Você ultrapassou a velocidade permitida, MULTADO')
+}else{
+    console.log('Você está dentro a velociade permitida')
+}
+console.log('Não esqueça o sinto de segurança.')
