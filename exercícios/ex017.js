7feb65f92e3fa80a63910a28c907fb98d98b40ee
Williamcodess <122636223+Williamcodess@users.noreply.github.com>
2023-03-21 15:01:21 -0300

    Continuação do exercício 17

--- exercícios/ex017.js
+++ exercícios/ex017.js
@@ -1,3 +1,20 @@
 let agr =[2, 5, 1, 3, 1]
+agr.push(7)
+agr.sort()
 
-console.log(`A representação do pokemon é ${agr}`)
+for(let poke in agr) {
+    console.log(`O índice ${poke} tem o valor \u{1F449} ${agr[poke]}`)
+}
+
+/* EXERCITANDO
+
+for (let poke=0; poke<agr.length; poke++) {
+    console.log(`O índice ${poke} tem valor \u{1F449}${agr[poke]}`)
+}
+
+var poke = 0
+while (poke < agr.length) {
+    console.log(`O índice ${poke} tem o valor \u{1F449} ${agr[poke]}`)
+}
+
+*/
